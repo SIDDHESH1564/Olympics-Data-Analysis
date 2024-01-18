e0771a45110275aac1cc3762111eecbc9ff57689
Siddhesh Khairnar <khairnarsiddhesh4057@gmail.com>
2024-01-18 20:34:59 +0530

    1st Commit

---
+++ preprocessor.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+
+
+def preprocess():
+    global df, region_df
+    # filtering for summer olympics
+    df = df[df['Season'] == 'Summer']
+    # merge with region_df
+    df = df.merge(region_df, on='NOC', how='left', suffixes=('', '_region'))
+    # dropping duplicates
+    df.drop_duplicates(inplace=True)
+    # one hot encoding medals
+    df2 = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
+
+    return df2
+
