b4e8128bc6594231a2c2a184a354907fc204a123
Siddhesh Khairnar <khairnarsiddhesh4057@gmail.com>
2024-01-19 01:07:47 +0530

    Fix ImportError in app.py: Removed problematic line causing ImportError in Plotly's create_distplot function. The line was at /mount/src/olympics-data-analysis/app.py, line 133. Updated the code to eliminate the dependency on create_distplot and resolved the issue. Also, added missing SciPy dependency to requirements.txt.

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ if user_menu == 'Athlete wise Analysis':
     x2=athlete_df[athlete_df['Medal']=='Gold']['Age'].dropna()
     x3=athlete_df[athlete_df['Medal']=='Silver']['Age'].dropna()
     x4=athlete_df[athlete_df['Medal']=='Bronze']['Age'].dropna()
-    fig=ff.create_distplot([x1,x2,x3,x4],['Overall Age','Gold Medalist','Silver Medalist','Bronze Medalist'],show_hist=False,show_rug=False)
+    #fig=ff.create_distplot([x1,x2,x3,x4],['Overall Age','Gold Medalist','Silver Medalist','Bronze Medalist'],show_hist=False,show_rug=False)
     #fig.update_layout(autosize=True,width=100,heigt=30)
     st.title("Distribution of Age")
     st.plotly_chart(fig)
